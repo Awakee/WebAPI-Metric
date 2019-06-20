57c60721ed64d11e10c56fa0362b1e33910f9a06
Awake <screamaion@yandex.ru>
2019-06-20 17:36:58 +0300

    Upload project

---
+++ WebApi MetricVisualization/Models/Metric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_MetricVisualization.Models
+{
+    public class Metric
+    {
+
+        public int Id;
+        public string Name;
+        public List<DateTime> Values = new List<DateTime>();
+
+    }
+}
